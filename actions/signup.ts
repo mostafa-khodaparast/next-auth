b38128c391594e532d3d4673a9b99c7c610522e3
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2024-02-04 13:03:52 +0330

    work on signUp action by saving signUp form data to db

--- actions/signup.ts
+++ actions/signup.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod'
 import { SignUpSchema } from '@/schemas'
+import bcrypt from 'bcrypt'
+import { db } from '@/prisma/db'
 
 
 export async function signUpAction(data: z.infer<typeof SignUpSchema>) {
@@ -12,6 +14,28 @@ export async function signUpAction(data: z.infer<typeof SignUpSchema>) {
     if (!validatedFields.success) {
         return { error: 'email or password is invalid' }
     }
+
+    const { name, email, password } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    //check unique email
+    const isUserInDB = await db.user.findUnique({
+        where: {
+            email
+        }
+    })
+    if (isUserInDB) return { error: 'This email already exists' }
+
+    //create user in db
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    //todo: send verification token email
 
     return { success: 'sign up successful' }
 }
